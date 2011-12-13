0f8d5f018ccba85bfa6cf296100cdbe581d36de2
Marcos Rodrigues <mrodrigues.uff@gmail.com>
2011-12-13 19:01:33 -0200

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Broadcaster::Application.config.secret_token = '1b1097859d0a8ed140693609720ea61bf281bf57912f245c395ba1205fe8f9b1a0a5f0b3cec3263fb6e3d7fd70000fec5f906b34cf55f3587ebc7f0bcff32989'
